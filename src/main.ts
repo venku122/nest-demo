343912cd806081b72a7744edd39e88fcb10d3131
T.J. Tarazevits <tarazevits@gmail.com>
2024-01-29 22:14:11 -0600

    Add OpenTelemetry tracing to nest project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import tracer from './tracer';
 
 async function bootstrap() {
+  tracer.start();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
---
+++ src/tracer.ts
@@ -0,0 +1,37 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { trace } from '@opentelemetry/api';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { containerDetector } from '@opentelemetry/resource-detector-container';
+import { envDetector, processDetector } from '@opentelemetry/resources';
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': {
+        requireParentSpan: true,
+      },
+    }),
+  ],
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter(),
+  }),
+  resourceDetectors: [containerDetector, envDetector, processDetector],
+  serviceName: process.env.OTEL_SERVICE_NAME,
+});
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
+
+export default sdk;
+
+export const tracer = trace.getTracer('nestjs');
